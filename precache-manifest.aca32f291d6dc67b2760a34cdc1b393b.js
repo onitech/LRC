b9ab4e5c249a7a6381b5b6be9fa0228f2c3e8b46
onitech <mc.busante@gmail.com>
2020-09-28 08:05:07 +0800

    Updates

---
+++ precache-manifest.aca32f291d6dc67b2760a34cdc1b393b.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b063d0d11097f2b890b120c24286e9a",
+    "url": "/LRC/index.html"
+  },
+  {
+    "revision": "9b862e56424f9c68fe5f",
+    "url": "/LRC/static/css/2.2d647298.chunk.css"
+  },
+  {
+    "revision": "a7066ee84dcc990bfc72",
+    "url": "/LRC/static/css/main.510f3613.chunk.css"
+  },
+  {
+    "revision": "9b862e56424f9c68fe5f",
+    "url": "/LRC/static/js/2.717a63b8.chunk.js"
+  },
+  {
+    "revision": "a02521f206cb8bac860a13141a9cb094",
+    "url": "/LRC/static/js/2.717a63b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7066ee84dcc990bfc72",
+    "url": "/LRC/static/js/main.443f54c5.chunk.js"
+  },
+  {
+    "revision": "4efa20154c633c11fa27",
+    "url": "/LRC/static/js/runtime-main.34e9e808.js"
+  },
+  {
+    "revision": "7874145ba837afc19f2b29bd410383db",
+    "url": "/LRC/static/media/Librarian1.7874145b.jpg"
+  },
+  {
+    "revision": "4ea5852abe9a3e37c454a48ae47454d2",
+    "url": "/LRC/static/media/Librarian2.4ea5852a.jpg"
+  },
+  {
+    "revision": "5a67b8b9628632528b1c1f31a8890152",
+    "url": "/LRC/static/media/Librarian3.5a67b8b9.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/LRC/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/LRC/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "cc49acd0654fa9e7d52bb139984ef713",
+    "url": "/LRC/static/media/destiny.cc49acd0.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/LRC/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/LRC/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/LRC/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/LRC/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/LRC/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/LRC/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/LRC/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/LRC/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/LRC/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/LRC/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/LRC/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/LRC/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/LRC/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/LRC/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/LRC/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "1997477e5e4d77619b34b3aa4102ef66",
+    "url": "/LRC/static/media/lrc1.3.1997477e.jpg"
+  },
+  {
+    "revision": "36fe3714f264f1bca014dcfa9df6a236",
+    "url": "/LRC/static/media/lrc2.1.36fe3714.jpg"
+  },
+  {
+    "revision": "2ded2f5ce8b7b53fb43d992af7c17f09",
+    "url": "/LRC/static/media/lrc3.1.2ded2f5c.jpg"
+  },
+  {
+    "revision": "404ccad11963625b1066dc2aa51fe1be",
+    "url": "/LRC/static/media/lrc4.1.404ccad1.jpg"
+  },
+  {
+    "revision": "653ebc9e9d3938700f436cdaaeb66af6",
+    "url": "/LRC/static/media/lrc5.1.653ebc9e.jpg"
+  },
+  {
+    "revision": "74293f4339a39615dbdf3df3361643c0",
+    "url": "/LRC/static/media/lrc6.74293f43.jpg"
+  },
+  {
+    "revision": "ed6ef92b0ba3e173c9c2de62bb60e6d3",
+    "url": "/LRC/static/media/lrc7.ed6ef92b.jpg"
+  },
+  {
+    "revision": "f24ba0b7844e14e36842e708e979355e",
+    "url": "/LRC/static/media/mini-logo.f24ba0b7.png"
+  }
+]);
